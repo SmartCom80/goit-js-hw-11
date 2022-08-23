9e49b0771386e6c6733554c7a06827d0ba910928
SmartCom80 <smartcom.ter@gmail.com>
2022-08-23 10:45:22 +0300

    added first markup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './src/sass/main';
+// import throttle from 'lodash.throttle';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import imageApi from './src/search-api.js';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// import templateFunction from './src/templates/photo-cards.hbs';
+// document.body.innerHTML = templateFunction();
